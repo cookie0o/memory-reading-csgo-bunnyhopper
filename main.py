cdc2f7122529f23f641980beb4211975f232a4c5
cookie0_o <81589649+cookie0o@users.noreply.github.com>
2022-02-08 00:39:58 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -18,55 +18,39 @@ from offsets import *
 #this is a memory reading bunnyhop for csgo :D
 def main():
 
-
 #wait if game is running and start hopping when it is
-    def check_if_csgo_running():
-        if "csgo.exe" in (p.name() for p in psutil.process_iter()) is True:
-            pm = pymem.Pymem("csgo.exe")
-            client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
-            
-            os.system("cls")
-            print (f"{Fore.CYAN}External csgo memory reading bunnyhopper;")
-            print(f"{Fore.LIGHTBLUE_EX}by: cookie0_o (cookie0o on github)")
+    os.system("cls")
+    print(f"{Fore.LIGHTMAGENTA_EX}PLS PRESS ENTER WHEN YOU ARE IN THE CS:GO MAIN MENU:")
+    input("")
+    pm = pymem.Pymem("csgo.exe")
+    client = pymem.process.module_from_name(pm.process_handle, "client.dll").lpBaseOfDll
+    
+    os.system("cls")
+    print (f"{Fore.CYAN}External csgo memory reading bunnyhopper;")
+    print(f"{Fore.LIGHTBLUE_EX}by: cookie0_o (cookie0o on github)")
 
-            #main hop part
-            while True:
-                try:
-                    #change console name to random str:
-                    random_str = secrets.token_urlsafe(1)
-                    ctypes.windll.kernel32.SetConsoleTitleW(random_str)
-
-                    
-                    #HOP
-                    if pm.read_int(client + dwLocalPlayer):
-                        player = pm.read_int(client + dwLocalPlayer)
-                        force_jump = client + dwForceJump
-                        on_ground = pm.read_int(player + m_fFlags)
-                        velocity = pm.read_float(player + m_vecVelocity)
-                        if keyboard.is_pressed("space") and on_ground == 257:
-                            if velocity < 1 and velocity > -1:
-                                pass
-                            else:
-                                pm.write_int(force_jump, 5)
-                                time.sleep(0.17)
-                                pm.write_int(force_jump, 4)
-                except:
-                    time.sleep(0.001)
-                    pass
-
-
-        else:
+    #main hop part
+    while True:
+        try:
             #change console name to random str:
-            random_str2 = secrets.token_urlsafe()
-            ctypes.windll.kernel32.SetConsoleTitleW(random_str2)
+            random_str = secrets.token_urlsafe(1)
+            ctypes.windll.kernel32.SetConsoleTitleW(random_str)
 
-
-            time.sleep(1)
-            os.system("cls")
-            print(f"{Fore.RED}START CSGO...")
-            check_if_csgo_running()
-
-
-
-    check_if_csgo_running()
+            
+            #HOP
+            if pm.read_int(client + dwLocalPlayer):
+                player = pm.read_int(client + dwLocalPlayer)
+                force_jump = client + dwForceJump
+                on_ground = pm.read_int(player + m_fFlags)
+                velocity = pm.read_float(player + m_vecVelocity)
+                if keyboard.is_pressed("space") and on_ground == 257:
+                    if velocity < 1 and velocity > -1:
+                        pass
+                    else:
+                        pm.write_int(force_jump, 5)
+                        time.sleep(0.17)
+                        pm.write_int(force_jump, 4)
+        except:
+            time.sleep(0.001)
+            pass
 main()
